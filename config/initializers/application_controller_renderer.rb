974de5b7a8f1136ae3747f22880f07852cc0efe7
macroshin <sckm0315@gmail.com>
2021-08-12 06:58:01 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fbb941c075c818379fc38d5c58457bad155166962c87d1e70e369d42b8b35c5c863fd44d323b0448d84103fa2975d688462547ea1ca74de6c02d048b5bad056d'
